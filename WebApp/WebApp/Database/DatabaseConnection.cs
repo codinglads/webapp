b2d55345fdbef2cc02d66459e38222e17f9753b7
Bradley Pirie <81576268+bppirie@users.noreply.github.com>
2022-02-23 17:47:22 -0500

    #N/A: Change to txt file

---
+++ WebApp/WebApp/Database/DatabaseConnection.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApp.Database
+{
+    /// <summary>
+    /// This class is the gateway by which our program will connect to a SQL Database
+    /// both locally and in the Azure Cloud
+    /// </summary>
+    public class DatabaseConnection
+    {
+
+    }
+}
